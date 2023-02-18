dc16ec977f9b19b888e0f0381faee1af05abad7b
Dave Cameron <dave@tinyweb.ca>
2023-02-18 10:25:16 -0800

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by Dave Cameron on 2023-02-18.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
